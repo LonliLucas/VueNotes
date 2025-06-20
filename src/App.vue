c890939de86e99751eda789d5f9d0c157b27536e
Lucas Gilles <lukej.gilless@gmail.com>
2025-06-20 12:14:30 -0600

    Finished up delete function

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,13 @@ const addTodo = () => {
     done: false,
     createdAt: new Date().getTime(),
   })
+
+  input_content.value = ''
+  input_category.value = null
+}
+
+const removeTodo = todo => {
+  todos.value = todos.value.filter(t => t !== todo)
 }
 
 watch(todos, newVal => {
@@ -34,6 +41,7 @@ watch(name, (newVal) => {
 
 onMounted(() => {
   name.value = localStorage.getItem('name') || ''
+  todos.value = JSON.parse(localStorage.getItem('todos')) || []
 })
 
 </script>
@@ -84,6 +92,30 @@ onMounted(() => {
 
           <input type="submit" value="Add todo" />
       </form>
+    </section>
+
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list">
+        
+        <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+
+          <label>
+            <input type="checkbox" v-model="todo.done" />
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content">
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+
+        </div>
+
+      </div>
     </section>
   </main>
 
